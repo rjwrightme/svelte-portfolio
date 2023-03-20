95e0b09252cc392de5728a4d57088934c98e19e8
RJ Wright <rj@rjwright.me>
2023-03-20 21:03:43 +1100

    Add Header component. Import Header to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
-  import rjLogo from "./assets/logo-main.svg";
+  import Header from "./lib/Header.svelte";
 </script>
 
 <main>
-  <img src={rjLogo} alt="RJ Logo" class="w-10" />
+  <Header />
   <h1 class="text-3xl font-bold text-center text-white">RJ Wright Portfolio</h1>
 </main>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  import rjLogo from "../assets/logo-main.svg";
+</script>
+
+<header>
+  <div
+    class="flex items-center justify-between md:max-w-screen-lg md:mx-auto px-6 md:px-10 py-4 md:py-6"
+  >
+    <img src={rjLogo} alt="RJ Logo" class="w-10" />
+    <ul class="flex">
+      <li class="text-white opacity-70 hover:opacity-100 text-lg px-4">
+        About
+      </li>
+      <li class="text-white opacity-70 hover:opacity-100 text-lg px-4">
+        Portfolio
+      </li>
+      <li class="text-white opacity-70 hover:opacity-100 text-lg px-4">
+        Contact
+      </li>
+    </ul>
+  </div>
+</header>
