0bb01a3b59b2bf578299e20d622827f0ac6afdb3
RJ Wright <rj@rjwright.me>
2023-03-25 22:06:29 +1100

    Add Brain.svelte component to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script>
   import Header from "./lib/Header.svelte";
+  import Brain from "./assets/Brain.svelte";
 </script>
 
 <main>
   <Header />
   <div class="bg-gradient-radial from-cyan-900 via-slate-900 to-slate-900">
-    <h1 class="text-3xl font-bold text-center text-white">
+    <h1 class="text-3xl font-bold text-center text-white py-10">
       Developer by Design.
     </h1>
+    <Brain />
   </div>
   <div>
     <h2>About Me</h2>
